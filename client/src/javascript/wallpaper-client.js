2520bb872116a64ed448294580d8879585cecf1e
Lucas Savva <lucas@vccomputers.ie>
2017-02-11 19:15:41 +0000

    Add notifications and multi cmd support

--- client/src/javascript/wallpaper-client.js
+++ client/src/javascript/wallpaper-client.js
@@ -1,4 +1,5 @@
 import { ipcRenderer as ipc } from 'electron';
+import $ from 'jquery';
 
 export default {
   set: path => ipc.send('set-wallpaper', path),
@@ -8,4 +9,18 @@ export default {
 
 ipc.on('set-wallpaper-done', (event, exitCode) => {
   console.log(`Background set. exit code ${exitCode}`);
+  if (exitCode === 0) {
+    $('#notification p')
+      .append('Background set!')
+      .parent().attr('class', 'alert-success');
+  } else {
+    $('#notification p')
+      .append(`Failed to set background, exit code ${exitCode}`)
+      .parent().attr('class', 'alert-danger');
+  }
+  $('#notification').addClass('alert alert-dismissable fade show');
+  setTimeout(
+    () => $('#notification').attr('class', 'alert fade hide'),
+    3000
+  );
 });
--- client/src/javascript/wallpaper-server.js
+++ client/src/javascript/wallpaper-server.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { reduce } from 'async';
 import { spawn } from 'child_process';
 import { ipcMain as ipc } from 'electron';
 import setters from '../config/wp_setters';
@@ -8,20 +9,26 @@ function escapePath(path) {
 }
 
 ipc.on('set-wallpaper', (event, path) => {
-  let i;
-  for (i = 0; i < setters.length; i++) {
-    const command = setters[i];
+  // Reduce the array to the command with the best exit code
+  // This allows the user to have multiple commands to set bg
+  reduce(setters.slice(), -1, (memo, command, cb) => {
     if (fs.existsSync(command.path)) {
       console.log(`Attempting to set wallpaper with ${command.path}`);
 
       const setter_process = spawn(command.path, command.args(escapePath(path)));
       setter_process.on('exit', (code) => {
         console.log(`Setter finished with exit code ${code}`);
-        event.sender.send('set-wallpaper-done', code);
+
+        // cb(errors, reduction)
+        cb(null, (code === 0 || memo === -1) ? code : memo);
       });
 
-      // Print the output of the command to the log for debugging
+      // Output the command's stdout to the console
       setter_process.stdout.on('data', data => console.log(data.toString()));
+    } else {
+      cb(null, memo);
     }
-  }
+  }, (err, exitCode) =>
+    event.sender.send('set-wallpaper-done', exitCode)
+  );
 });
